6347f3d07c3c07537f7535e77f3caccfb4313080
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-05-04 14:02:21 +0200

    da modificare funzione in caso di bomba ferma tutto

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const start = document.getElementById("start");
 // prendo la griglia
 const griglia = document.querySelector(".flex");
 
+// variabili che mi serviranno per il punteggio, per sapere le celle di ogni livello di difficoltà almeno creo funzioni più generiche e una array vuoto per pe bombe
+let punteggio = 0;
+let n;
+let listBomb = [];
+
 // sul click del pulsante
 start.addEventListener('click', function () {
 
@@ -14,60 +19,120 @@ start.addEventListener('click', function () {
     // prendo i valori del select
     const livello = document.getElementById("difficoltà").value;
 
-    // variabile che mi servirà per dire quante celle mi servono
-    let n;
 
 
+    // nel caso in cui fosse facile
     if (livello == "easy") {
-        // assegno alla griglia la width per un 10x10
+        // assegno alla griglia la width per un 10x10 e ad n il valore delle celle presenti
         griglia.classList.add("w10");
         n = 100;
-        square(n);
+        // creo la lista di bombe in base a n
+        listBomb = creaBombe(n);
+        // per n volte
+        for (let i = 1; i <= n; i++) {
+            // crea un quadrato e inseriscilo nella griglia
+            square = creaQuadrato();
+            griglia.appendChild(square)
+            // se il quadrato è incluso nella lista creata precedentemente
+            if (listBomb.includes(i)) {
+                // ad esso aggiungi la classe bomb
+                square.classList.add("bomb")
+            }
+            // per ogni quadrato creato aggiungo un eventlistener con la funzione click
+            square.addEventListener("click", function () {
+                // click this mi serve per intendere il singolo quadrato cliccato senno' mi si riferisce sempre al 100esimo square creato
+                click(this, n);
+                // once true per rendere i quadrati cliccabili una sola volta
+            }, { once: true });
+        }
+
+
     } else if (livello == "normal") {
         griglia.classList.add("w9");
         n = 81;
-        square(n);
+        listBomb = creaBombe(n);
+        for (let i = 1; i <= n; i++) {
+            square = creaQuadrato();
+            griglia.appendChild(square)
+            if (listBomb.includes(i)) {
+                square.classList.add("bomb")
+            }
+            square.addEventListener("click", function () {
+                click(this, n);
+            }, { once: true });
+        }
     } else if (livello == "hard") {
         griglia.classList.add("w7");
         n = 49;
-        square(n);
+        listBomb = creaBombe(n);
+        for (let i = 1; i <= n; i++) {
+            square = creaQuadrato();
+            griglia.appendChild(square)
+            if (listBomb.includes(i)) {
+                square.classList.add("bomb")
+            }
+            square.addEventListener("click", function () {
+                click(this, n);
+            }, { once: true });
+        }
     }
 })
 
 
 
-function square(n) {
-    for (let i = 1; i <= n; i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        griglia.appendChild(square);
-        click(square)
-    }
-
+// funzione crea bomba in base a n(il numero dei quadrati)
+function creaBombe(n) {
     let listBomb = [];
-
+    // finche la lista ha meno di 16 elementi crea un  numero random, se non è già incluso nella lista push al suo interno
     while (listBomb.length < 16) {
         let x = Math.floor(Math.random() * n + 1);
         if (!listBomb.includes(x)) {
             listBomb.push(x);
-            griglia.querySelector(`.square:nth-child(${x})`).classList.add("bomb");
+        }
+    }
+    return listBomb;
+}
+
+
+// funzione crea quadrato
+function creaQuadrato() {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    return square;
+}
+
+
+// funzione che mi controlla il click
+function click(square, n) {
+    // il numero massimo di punti arrivabile
+    let maxPoints = n - 16;
+    // se l'elemento cliccato contiene la classe bomb
+    if (square.classList.contains("bomb")) {
+        // prendi tutti gli elementi con la classe bomb e a ciascuno di lor aggiungi la classe red
+        const allBobms = document.querySelectorAll(".bomb");
+        allBobms.forEach(function (bomb) {
+            bomb.classList.add("red")
+        })
+        console.log("you lose");
+
+    } else {
+        // altrimenti aggiungi la classe checked e aggiugni +1 al punteggio,la vittoria si ottiene solo al raggiungimento del massimo punteggio
+        square.classList.add("checked")
+        punteggio += 1;
+        if (punteggio == maxPoints) {
+            console.log("you  win " + punteggio)
+        } else {
+            console.log(punteggio)
         }
     }
 }
 
 
-function click(square) {
-    let point = 0;
-    square.addEventListener('click', function () {
-        if (square.classList.contains("bomb")) {
-            console.log("you lose");
-            square.classList.add("red");
-        } else {
-            square.classList.add("checked");
-            point += 1;
-        }
-    })
-}
+// da modificare ,in caso di "bomba" non rendere possibile il click di altri elementi
+// const allSquares = document.querySelectorAll(".square");
+// allSquares.forEach(function (square) {
+//     square.removeEventListener("click", click);
+// });
 
 
 
@@ -79,48 +144,3 @@ function click(square) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function square(n) {
-//     for (let i = 1; i <= n; i++) {
-//         let square = document.createElement("div");
-//         square.classList.add("square");
-//         griglia.appendChild(square);
-//     }
-
-// }
-
-// function click(square) {
-//     square.addEventListener('click', function () {
-//         if (!square.classList.contains("bomb")) {
-//             console.log("you lose")
-//         }
-//     })
-// }
-
-
-// function bomb(n, griglia) {
-//     let listBomb = [];
-//     while (listBomb.length < 16) {
-
-//         let x = Math.floor(Math.random() * n + 1);
-//         if (!listBomb.includes(x)) {
-//             listBomb.push(x);
-//             griglia.querySelector(`.square:nth-child(${x})`).classList.add("bomb");
-//         }
-//     }
-// }
